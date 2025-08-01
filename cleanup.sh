9925da37574c406a50dac26f85a8dc944d137101
Manjakasoa <manjakaranja@gmail.com>
2025-08-01 10:08:56 +0000

    Additional commit 01/08/25

---
+++ cleanup.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+
+echo "Suppression des dossiers .ipynb_checkpoints..."
+find . -type d -name ".ipynb_checkpoints" -exec rm -rf {} +
+
+echo "Suppression du cache pip..."
+rm -rf ~/.cache/pip
+
+echo "Nettoyage des caches Conda..."
+conda clean --all --yes
+
+echo "Nettoyage Docker..."
+docker system prune -a --volumes --force
+
+echo "Script terminé. Pense à redémarrer ton Codespace si possible."
+
+df -h
+
+# ./cleanup.zsh TO RUN IT
